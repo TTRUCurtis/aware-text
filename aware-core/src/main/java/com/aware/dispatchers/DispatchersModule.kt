a370d33d215698460f0417fcbaba37446e208aba
Olivia <oliviadodge@gmail.com>
2022-06-29 13:17:59 -0700

    Launch sms Plugin work on a coroutine which uses an IO Dispatcher

---
+++ aware-core/src/main/java/com/aware/dispatchers/DispatchersModule.kt
@@ -0,0 +1,16 @@
+package com.aware.dispatchers
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+@Module
+@InstallIn(SingletonComponent::class)
+class DispatchersModule {
+
+    @Provides
+    fun providesIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+}
